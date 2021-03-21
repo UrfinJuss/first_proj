7093415ef3cb8cff611eca4b7b8a2116815e089d
UrfinJuss <73132253+UrfinJuss@users.noreply.github.com>
2021-03-21 22:34:30 +0300

    Update views.py

--- conf/tours/views.py
+++ conf/tours/views.py
@@ -1,25 +1,33 @@
-from django.shortcuts import render
-from django.http import HttpResponseBadRequest, HttpResponseNotFound, HttpResponseForbidden, HttpResponseServerError
-from .data import tours, title, subtitle, description, departures
 import random
+
+from django.shortcuts import render
+from django.http import HttpResponseBadRequest, HttpResponseNotFound,\
+    HttpResponseForbidden, HttpResponseServerError
+
+from .data import tours, title, subtitle, description, departures
+
 
 
 def main_view(request):
+    random_tours = dict(random.sample(tours.items(), 6))
     return render(request, 'index.html', context={
         'title': title,
         'subtitle': subtitle,
         'description': description,
-        'tours': tours,
+        'tours': random_tours,
 
     })
 
 
 def departure_view(request, departure):
-    random_tours = dict(random.sample(tours.items(), 6))
-    list_prices = []
-    list_days = []
-    list_test = [i for i in random_tours.values()]
-    for i in range(6):
+    if departure not in departures:
+        return HttpResponseNotFound('<h1>Page not found</h1>')
+    dep_tours = {}
+    for tour_id, tour_name in tours.items():
+        if tour_name['departure'] == departure:
+            dep_tours[tour_id] = tour_name
+    list_prices, list_days, list_test = [], [], [i for i in dep_tours.values()]
+    for i in range(len(list_test)):
         list_prices.append(list_test[i]['price'])
         list_days.append(list_test[i]['nights'])
     list_prices.sort(), list_days.sort()
@@ -27,19 +35,20 @@ def departure_view(request, departure):
         'title': title,
         'subtitle': subtitle,
         'description': description,
-        'random_tours': random_tours,
+        'dep_tours': dep_tours,
         'departure': departures[departure],
-        'maxprice': list_prices[5],
+        'maxprice': list_prices[len(list_test) - 1],
         'minprice': list_prices[0],
-        'maxday': list_days[5],
+        'maxday': list_days[len(list_test) - 1],
         'minday': list_days[0],
 
     })
 
 
 def tour_view(request, id):
+    if id not in tours:
+        return HttpResponseNotFound('<h1>Page not found</h1>')
     return render(request, 'tour.html', context={
-        'departures': departures,
         'title': title,
         'subtitle': subtitle,
         'description': description,
